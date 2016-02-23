8374eb22ca7a8599d8b8336dbbb2641cefe1eaa9
Steve Bauman <steven_bauman@outlook.com>
2016-02-23 16:18:34 -0500

    Added extra validation on user update job
    - Users who are the only administrator and are trying to remove all
    roles will be shown an error message to prevent the removal of the only
    administrator

--- src/Jobs/Admin/User/Update.php
+++ src/Jobs/Admin/User/Update.php
@@ -2,9 +2,12 @@
 
 namespace App\Jobs\Admin\User;
 
-use App\Http\Requests\UserRequest;
+use App\Exceptions\Admin\CannotRemoveRolesException;
+use App\Http\Requests\Admin\UserRequest;
 use App\Jobs\Job;
+use App\Models\Role;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class Update extends Job
 {
@@ -33,6 +36,8 @@ class Update extends Job
     /**
      * Execute the job.
      *
+     * @throws CannotRemoveRolesException
+     *
      * @return bool
      */
     public function handle()
@@ -40,10 +45,40 @@ class Update extends Job
         $this->user->name = $this->request->input('name', $this->user->name);
         $this->user->email = $this->request->input('email');
 
-        if ($this->request->has('password')) {
+        $password = $this->request->input('password');
+
+        // Verify before changing the users password that it's not empty.
+        if (!empty($password)) {
             $this->user->password = bcrypt($this->request->input('password'));
         }
 
-        return $this->user->save();
+        $roles = $this->request->input('roles', []);
+
+        // Retrieve all administrators.
+        $administrators = $this->user->whereHas('roles', function (Builder $builder) {
+            $builder->whereName('administrator');
+        })->get();
+
+
+        $admin = Role::whereName('administrator')->first();
+
+        // We need to verify that if the user is trying to remove all roles on themselves,
+        // and they are the only administrator, that we throw an exception notifying them
+        // that they can't do that. Though we want to allow the user to remove the
+        // administrator role if more than one administrator exists.
+        if (count ($roles) === 0
+            && $this->user->hasRole($admin)
+            && $this->user->getKey() === auth()->user()->getKey()
+            && count($administrators) === 1) {
+            throw new CannotRemoveRolesException("Unable to remove the administrator role. You're the only administrator.");
+        }
+
+        if ($this->user->save()) {
+            $this->user->roles()->sync($roles);
+
+            return true;
+        }
+
+        return false;
     }
 }
