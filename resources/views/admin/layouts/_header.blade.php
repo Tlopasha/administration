4b2e9885d46ca23c2332854dfe18dab07ba0242e
Steve Bauman <steven_bauman@outlook.com>
2016-02-24 10:34:18 -0500

    Moved resource assets to admin folder

--- resources/views/admin/layouts/_header.blade.php
+++ resources/views/admin/layouts/_header.blade.php
@@ -6,25 +6,25 @@
 <meta name="author" content="Author">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<link rel="stylesheet" href="{{ asset('css/libs/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/libs/font-awesome.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/libs/select2.css') }}">
-<link rel="stylesheet" href="{{ asset('css/libs/select2-bootstrap.css') }}">
-<link rel="stylesheet" href="{{ asset('css/libs/sweetalert.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/libs/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/libs/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/libs/select2.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/libs/select2-bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/libs/sweetalert.css') }}">
 
 <!-- Custom Application CSS -->
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('admin/css/app.css') }}">
 
-<script type="text/javascript" src="{{ asset('js/libs/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/libs/jquery.pjax.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/libs/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/libs/select2.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/libs/sweetalert.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/jquery.pjax.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/select2.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/sweetalert.min.js') }}"></script>
 
 <!-- Custom Application JS -->
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/app.js') }}"></script>
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
 <!--[if lt IE 9]>
-<script type="text/javascript" src="{{ asset('js/libs/html5.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin/js/libs/html5.js') }}"></script>
 <![endif]-->
