2bf7d6487298627ac4c8679138f3fdefa77412dc
Steve Bauman <steven_bauman@outlook.com>
2016-02-24 11:01:08 -0500

    Added more default permissions

--- src/Migrations/2014_10_12_300000_seed_roles_and_permissions.php
+++ src/Migrations/2014_10_12_300000_seed_roles_and_permissions.php
@@ -18,6 +18,7 @@ class SeedRolesAndPermissions extends Migration
             'label' => 'Administrator',
         ]);
 
+        // User Permissions
         Permission::firstOrCreate([
             'name' => 'admin.users.index',
             'label' => 'View All Users',
@@ -43,6 +44,58 @@ class SeedRolesAndPermissions extends Migration
             'label' => 'Delete Users',
         ]);
 
+        // Role Permissions
+        Permission::firstOrCreate([
+            'name' => 'admin.roles.index',
+            'label' => 'View All Roles',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.roles.create',
+            'label' => 'Create Roles',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.roles.edit',
+            'label' => 'Edit Roles',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.roles.show',
+            'label' => 'View Roles',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.roles.destroy',
+            'label' => 'Delete Roles',
+        ]);
+
+        // Permission Permissions
+        Permission::firstOrCreate([
+            'name' => 'admin.permissions.index',
+            'label' => 'View All Permissions',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.permissions.create',
+            'label' => 'Create Permissions',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.permissions.edit',
+            'label' => 'Edit Permissions',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.permissions.show',
+            'label' => 'View Permissions',
+        ]);
+
+        Permission::firstOrCreate([
+            'name' => 'admin.permissions.destroy',
+            'label' => 'Delete Permissions',
+        ]);
+
         $administrator->grant(Permission::all());
     }
 
@@ -55,10 +108,25 @@ class SeedRolesAndPermissions extends Migration
     {
         Role::whereName('administrator')->delete();
 
+        // Delete User Permissions
         Permission::whereName('admin.users.index')->delete();
         Permission::whereName('admin.users.create')->delete();
         Permission::whereName('admin.users.edit')->delete();
         Permission::whereName('admin.users.show')->delete();
         Permission::whereName('admin.users.destroy')->delete();
+
+        // Delete Role Permissions
+        Permission::whereName('admin.roles.index')->delete();
+        Permission::whereName('admin.roles.create')->delete();
+        Permission::whereName('admin.roles.edit')->delete();
+        Permission::whereName('admin.roles.show')->delete();
+        Permission::whereName('admin.roles.destroy')->delete();
+
+        // Delete Permission Permissions
+        Permission::whereName('admin.permissions.index')->delete();
+        Permission::whereName('admin.permissions.create')->delete();
+        Permission::whereName('admin.permissions.edit')->delete();
+        Permission::whereName('admin.permissions.show')->delete();
+        Permission::whereName('admin.permissions.destroy')->delete();
     }
 }
