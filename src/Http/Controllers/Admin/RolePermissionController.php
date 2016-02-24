800e5c2d0bd86d9796930b601791c405340986b9
Steve Bauman <steven_bauman@outlook.com>
2016-02-24 11:59:03 -0500

    Added new resource controllers

---
+++ src/Http/Controllers/Admin/RolePermissionController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steve
+ * Date: 2/24/2016
+ * Time: 11:28 AM
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class RolePermissionController
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function store($roleId)
+    {
+        //
+    }
+
+    public function destroy($roleId, $permissionId)
+    {
+        //
+    }
+}
---
+++ src/Http/Controllers/Admin/RoleUserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class RoleUserController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function store($roleId)
+    {
+        //
+    }
+
+    public function destroy($roleId, $userId)
+    {
+        //
+    }
+}
