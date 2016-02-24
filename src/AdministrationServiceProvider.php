45fee5e746320b22bf32afbb8f96d1febc69c903
Steve Bauman <steven_bauman@outlook.com>
2016-02-24 10:32:22 -0500

    Added HTML Service Provider

--- src/AdministrationServiceProvider.php
+++ src/AdministrationServiceProvider.php
@@ -3,9 +3,6 @@
 namespace Stevebauman\Administration;
 
 use Illuminate\Support\ServiceProvider;
-use Orchestra\Html\Form\Factory as FormFactory;
-use Orchestra\Html\Table\Factory as TableFactory;
-use Orchestra\Html\HtmlServiceProvider;
 
 class AdministrationServiceProvider extends ServiceProvider
 {
@@ -79,30 +76,6 @@ class AdministrationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(HtmlServiceProvider::class);
-
-        // Bind the form factory.
-        $this->app->bind('Orchestra\Contracts\Html\Form\Factory', function ($app) {
-            $factory = new FormFactory($app);
-
-            $factory->setConfig([
-                'format' => '<span class="label label-danger">:message</span>',
-                'view' => 'admin.components.form',
-            ]);
-
-            return $factory;
-        });
-
-        // Bind the table factory.
-        $this->app->bind('Orchestra\Contracts\Html\Table\Factory', function ($app) {
-            $factory = new TableFactory($app);
-
-            $factory->setConfig([
-                'empty' => 'There are no records to display.',
-                'view' => 'admin.components.table',
-            ]);
-
-            return $factory;
-        });
+        //
     }
 }
